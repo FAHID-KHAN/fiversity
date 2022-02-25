d2f32a677dafe5a1f4b96eaf2223cb83cd2ad495
Macbook <macbook@Macbooks-MBP.home>
2022-02-25 16:24:26 +0200

    Components section update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Fiversity</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const Navbar = () => {
+    return(
+        <div>
+            <h1>Fiversity</h1>
+        </div>
+    )
+}
+
+export default Navbar;
