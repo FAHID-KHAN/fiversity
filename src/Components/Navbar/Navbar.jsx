73fa61aefdb2fe80413cec5c868e6ffc2cdfb563
Fahid A. Khan <fahidkhan@Fahids-MBP.home>
2022-03-04 01:15:18 +0200

    Navbar work started

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import react from 'react';
+import "./Navbar.css";
+
+const Navbar = () => {
+    return(
+        <div className='topnav'>
+           
+            <a className='active'>Home</a>
+            <a className='#news'>News</a>
+            <a className='#contact'>Contact</a>
+            <a className='#about'>About</a>
+        <span className="Header">Fiversity</span>
+        </div>
+    )
+}
+
+export default Navbar;
+
+//This is a smiple comment 
+//adding another comment 
