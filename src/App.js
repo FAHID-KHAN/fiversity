cd28ae254a4d77fe7f1702dd13dc3f7a53c79016
Mafruha Leavin <fahidkhan@Fahids-MacBook-Pro-2.local>
2022-03-22 19:53:19 +0200

    sub pages added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import Home from "./Home";
+import About from "./About";
+import Service from "./Service";
+import Contact from "./Contact";
+import Footer from "./Footer";
+import {Route, Switch, Redirect} from "react-router-dom";
+import Navbar from "./Navbar";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <>
+    <Navbar />
+    <Switch>
+      <Route exact path="/"  component={Home} />
+      <Route exact path="/about"  component={About} />
+      <Route exact path="/service"  component={Service} />
+      <Route exact path="/contact"  component={Contact} />
+      <Redirect to="/" />
+      <Home />
+    </Switch>
+    <Footer />
+
+    </>
   );
-}
+};
 
 export default App;
-//Simple comment to check something 
