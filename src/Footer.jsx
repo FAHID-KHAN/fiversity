9b548624af7276e2a3d6fed4a7a0f914f2de9f59
Fahid A. Khan <fahidkhan@Fahids-MBP.home>
2022-03-23 23:03:13 +0200

    Footer word change

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 
 const Footer = () => {
-    return(
-        <>
-            <footer className="w-100 bg-light text-center ">
-            <p> 2021 React Website. All Rights Reserved | Terms and Conditions</p>
-
-            </footer>
-        </>
-    )
+  return (
+    <>
+      <footer className="w-100 bg-light text-center ">
+        <p>
+          {" "}
+          2022 @FiVersity. All Rights Reserved by Fahid and Mafruha | Terms and
+          Conditions
+        </p>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
